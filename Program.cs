575b3f7615a7ccc599218dd806f37bacf7672616
Antonio L <256megas@gmail.com>
2022-09-14 17:53:28 +0200

    read from keyboard

--- Program.cs
+++ Program.cs
@@ -6,8 +6,15 @@ namespace FirstConsoleProject
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, I'm a computer, I'm send from the future.\n");
+            int num01;
+            int num02;
+            Console.Write("Type a number to be multiplied: ");
+            num01 = Convert.ToInt32( Console.ReadLine());
+            Console.Write("Type another number: ");
+            num02 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("The result is: " + num01*num02);
             Console.ReadKey();
+
         }
     }
 }
